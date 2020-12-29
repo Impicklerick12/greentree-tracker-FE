4230e8084067fcb2833dc28716e37a1f4e911885
Tyler Hall <tylerhall12@outlook.com>
2020-12-29 14:44:07 +1000

    .

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -59,8 +59,8 @@ const Admin = ({history}) => {
                 <Grid item xs={12} sm={6}>
                     <Paper className={classes.paper}>
                         <Typography variant="h2">Quote Requests</Typography>
-                        {submittedQuotes.map((quote) => 
-                            <SubmittedQuotes key={quote._id} quote={quote} />
+                        {submittedQuotes.map((quote, i) => 
+                            <SubmittedQuotes key={i} quote={quote} />
                         )}
                     </Paper>
                 </Grid>
