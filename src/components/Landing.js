4ef79aacd8d1e03613c0575ff9f2a7eff7388670
katrinamarquez <katrina.l.marquez@gmail.com>
2021-01-07 14:39:02 +1000

    update with image carousel

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import image1 from "../images/aiokr-chen-YtqUWAlL2GE-unsplash.jpg";
+import image2 from "../images/artem-kniaz-03Dhzux8mCI-unsplash.jpg";
+import image3 from "../images/george-bakos-SEFaaIjrjZA-unsplash.jpg";
 
 const Landing = () => {
     return (
-        <div>
-            <h1>Home/Landing Page</h1>
+        <div className="imageCarousel">
+        <AliceCarousel autoPlay autoPlayInterval="3000">
+            <img src={image1} className="sliderimg" width="70%" height="70%" alt="plant image 1"/>
+            <img src={image2} className="sliderimg" width="70%" height="70%" alt="plant image 2"/>
+            <img src={image3} className="sliderimg" width="70%" height="70%" alt="plant image 3"/>
+        </AliceCarousel>
         </div>
     )
 }
