624abf92f7fcb47c41c73861db2a758dfaa50d82
Tyler Hall <tylerhall12@outlook.com>
2021-01-10 13:15:33 +1000

    Styling to searchbar

--- src/components/FilterOptions.js
+++ src/components/FilterOptions.js
@@ -9,7 +9,8 @@ import {
     FormControlLabel,
     FormControl,
     FormLabel,
-    Button
+    Button,
+    Typography
 } from '@material-ui/core'
 
 import SearchIcon from '@material-ui/icons/Search';
@@ -72,38 +73,20 @@ const useStyles = makeStyles((theme) => ({
     },
     filterButtons: {
         display: 'flex',
+        flexDirection: 'column',
         [theme.breakpoints.down('sm')]: {
             width: '100%',
+            flexDirection: 'row',
             justifyContent: 'space-between'
         },
+    },
+    buttons: {
+        margin: theme.spacing(0.5)
     },
     checked: {}
   }));
 
 const FilerOptions = ({filterOptions}) => {
-    const { dispatch } = useGlobalState()
-    
-    // const [treeChecked, setTreeChecked] = useState(false)
-    // const [shrubChecked, setShrubChecked] = useState(false)
-    // const [grassChecked, setGrassChecked] = useState(false)
-    // const [groundCoverChecked, setGroundCoverChecked] = useState(false)
-    // // Set initial state. Pot size 140mm
-    // const [smallPotChecked, setSmallPotChecked] = useState(false)
-    // // Set initial state. Pot size 250mm
-    // const [mediumPotChecked, setMediumPotChecked] = useState(false)
-    // // Set initial state. Pot size 350mm
-    // const [largePotChecked, setLargePotChecked] = useState(false)
-    // const [specialChecked, setSpecialChecked] = useState(false)
-    // // Price range $0 to $50
-    // const [rangeOneChecked, setRangeOneChecked] = useState(false)
-    // // Price range $50 to $100
-    // const [rangeTwoChecked, setRangeTwoChecked] = useState(false)
-    // // Price range $100 and over
-    // const [rangeThreeChecked, setRangeThreeChecked] = useState(false)
-
-    const [categoryValue, setCategoryValue] = useState(null);
-    const [potSizeValue, setPotSizeValue] = useState(null);
-    const [priceValue, setPriceValue] = useState(null);
     // const [searchValue, setSearchValue] = useState(null)
 
     const initialFilterState = {
@@ -122,16 +105,6 @@ const FilerOptions = ({filterOptions}) => {
             ...filters,
             [filterType]: filterValue
         })
-
-        // if (filterValue == 50) {
-        //     filterValue = { $lt: 50 }
-        // } else if (filterValue == 100) {
-        //     filterValue = { $lt: 100 }
-        // } else if (filterValue == "100+") {
-        //     filterValue = { $gte: 100}
-        // } else {
-        //     filterValue = filterValue
-        // }
     }
     
     const submitFilters = () => {
@@ -163,8 +136,6 @@ const FilerOptions = ({filterOptions}) => {
             data: searchValue
         })
     }*/}
-
-    // Links to material UI set up
     const classes = useStyles()
 
     const categoryRadio = {
@@ -176,11 +147,11 @@ const FilerOptions = ({filterOptions}) => {
 
     const potSizeRadio = ["140mm", "250mm", "350mm"]
 
-    const priceRadio = {
-        "Less than $50": "50",
-        "Less than $100": "99",
-        "More than $100": "100"
-    }
+    // const priceRadio = {
+    //     "Less than $50": "50",
+    //     "Less than $100": "99",
+    //     "More than $100": "100"
+    // }
 
     const createRadio = (obj) => {
         let array = []
@@ -191,120 +162,11 @@ const FilerOptions = ({filterOptions}) => {
     }
 
     return (
-        // <Grid container>
-        //     <Grid item className={classes.container}>
-        //         <Paper className={classes.paper}>
-        //             <div className={classes.radioChecks}>
-        //                 <div className={classes.category}>
-        //                     <FormControl component="fieldset">
-        //                         <FormLabel component="legend">Category</FormLabel>
-        //                         <RadioGroup row aria-label="Category" name="category" onChange={handleChange}>
-        //                             <FormControlLabel 
-        //                                 checked={treeChecked} 
-        //                                 onClick={() => setTreeChecked(!treeChecked)}
-        //                                 value="tree" 
-        //                                 control={<Radio />} 
-        //                                 label="Tree" 
-        //                             />
-        //                             <FormControlLabel 
-        //                                 checked={shrubChecked}
-        //                                 onClick={() => setShrubChecked(!shrubChecked)} 
-        //                                 value="shrub" 
-        //                                 control={<Radio />} 
-        //                                 label="Shrub" 
-        //                             />
-        //                             <FormControlLabel 
-        //                                 checked={grassChecked} 
-        //                                 onClick={() => setGrassChecked(!grassChecked)}
-        //                                 value="grass" 
-        //                                 control={<Radio />} 
-        //                                 label="Grass" 
-        //                             />
-        //                             <FormControlLabel 
-        //                                 checked={groundCoverChecked}
-        //                                 onClick={() => setGroundCoverChecked(!groundCoverChecked)} 
-        //                                 value="ground_cover" 
-        //                                 control={<Radio />} 
-        //                                 label="Ground Cover" 
-        //                             />
-        //                         </RadioGroup>
-        //                     </FormControl>
-        //                     <FormControl component="fieldset">
-        //                         <FormLabel component="legend">Price</FormLabel>
-        //                         <RadioGroup row aria-label="Price" name="price" onChange={handleChange}>
-        //                             <FormControlLabel 
-        //                                 checked={rangeOneChecked}
-        //                                 onClick={() => setRangeOneChecked(!rangeOneChecked)}
-        //                                 value="$0 to $50" 
-        //                                 control={<Radio />} 
-        //                                 label="$0 to $50" 
-        //                             />
-        //                             <FormControlLabel 
-        //                                 checked={rangeTwoChecked}
-        //                                 onClick={() => setRangeTwoChecked(!rangeTwoChecked)}
-        //                                 value="$50 to $100" 
-        //                                 control={<Radio />} 
-        //                                 label="$50 to $100" 
-        //                             />
-        //                             <FormControlLabel 
-        //                                 checked={rangeThreeChecked}
-        //                                 onClick={() => setRangeThreeChecked(!rangeThreeChecked)}
-        //                                 value="$100 and above" 
-        //                                 control={<Radio />} 
-        //                                 label="$100 and above" 
-        //                             />
-        //                         </RadioGroup>
-        //                     </FormControl>
-        //                 </div>
-        //             </div>
-        //              <div className={classes.potSize}>
-        //                     <FormControl component="fieldset">
-        //                         <FormLabel component="legend">Pot Size</FormLabel>
-        //                         <RadioGroup row aria-label="Pot Size" name="pot_size" onChange={handleChange}>
-        //                             <FormControlLabel 
-        //                                 checked={smallPotChecked}
-        //                                 onClick={() => setSmallPotChecked(!smallPotChecked)}
-        //                                 value="140mm" 
-        //                                 control={<Radio />} 
-        //                                 label="140mm" 
-        //                             />
-        //                             <FormControlLabel 
-        //                                 checked={mediumPotChecked}
-        //                                 onClick={() => setMediumPotChecked(!mediumPotChecked)}
-        //                                 value="250mm" 
-        //                                 control={<Radio />} 
-        //                                 label="250mm" 
-        //                             />
-        //                             <FormControlLabel 
-        //                                 checked={largePotChecked}
-        //                                 onClick={() => setLargePotChecked(!largePotChecked)}
-        //                                 value="350mm" 
-        //                                 control={<Radio />} 
-        //                                 label="350mm" 
-        //                             />
-        //                         </RadioGroup>
-        //                     </FormControl>
-        //                     <FormControl component="fieldset">
-        //                         <FormLabel component="legend">Seasonal</FormLabel>
-        //                         <RadioGroup row aria-label="Special" name="special" onChange={handleChange}>
-        //                             <FormControlLabel 
-        //                                 checked={specialChecked}
-        //                                 onClick={() => setSpecialChecked(!specialChecked)}
-        //                                 value="Seasonal" 
-        //                                 control={<Radio />} 
-        //                                 label="Seasonal" 
-        //                             />
-        //                         </RadioGroup>
-        //                     </FormControl>
-        //                 </div>
-        //         </Paper>
-        //     </Grid>
-        // </Grid>
-
         <Grid container>
             <Grid item className={classes.container}>
-                <Paper className={classes.paper}>
-                    <div>
+                <Paper>
+                    {/* SEARCH FUNCTION FIELD */}
+                    {/* <div>
                         <div className={classes.search}>
                             <div className={classes.searchIcon}>
                                 <SearchIcon />
@@ -318,37 +180,26 @@ const FilerOptions = ({filterOptions}) => {
                                 inputProps={{ 'aria-label': 'search' }}
                             />
                         </div>
-                    </div>
-                    <div className={classes.radioChecks}>
-                        <div className={classes.category}>
+                    </div> */}
+                    <Grid container className={classes.paper}>
+                        <Grid className={`${classes.category} ${classes.radioChecks}`}>
                             <FormControl component="fieldset">
                                 <FormLabel component="legend">Category</FormLabel>
                                 <RadioGroup row aria-label="Category" name="category" value={filters.category} onChange={handleFilterChange}>
-                                    
                                     { createRadio(categoryRadio) }
-
-                                    {/* <FormControlLabel value="tree" control={<Radio />} label="Tree" />
-                                    <FormControlLabel value="shrub" control={<Radio />} label="Shrub" />
-                                    <FormControlLabel value="grass" control={<Radio />} label="Grass" />
-                                    <FormControlLabel value="ground cover" control={<Radio />} label="Ground Cover" /> */}
                                 </RadioGroup>
                             </FormControl>
-                        </div>
-                        <div className={classes.potSize}>
+                        </Grid>
+                        <Grid className={`${classes.potSize} ${classes.radioChecks}`}>
                             <FormControl component="fieldset">
                                 <FormLabel component="legend">Pot Size</FormLabel>
                                 <RadioGroup row aria-label="Pot Size" name="pot_size" value={filters.pot_size} onChange={handleFilterChange}>
-                                    
                                     { potSizeRadio.map( pot => {
                                         return <FormControlLabel value={pot} control={<Radio />} label={pot} />
                                     })}
-                                    
-                                    {/* <FormControlLabel value="140mm" control={<Radio />} label="140mm" />
-                                    <FormControlLabel value="250mm" control={<Radio />} label="250mm" />
-                                    <FormControlLabel value="350mm" control={<Radio />} label="350mm" /> */}
                                 </RadioGroup>
                             </FormControl>
-                        </div>
+                        </Grid>
                         {/* PRICE RADIO BUTTONS */}
                         {/* <div className={classes.price}>
                             <FormControl component="fieldset">
@@ -358,22 +209,24 @@ const FilerOptions = ({filterOptions}) => {
                                 </RadioGroup>
                             </FormControl>
                         </div> */}
-                    </div>
-                    <Grid className={classes.filterButtons}>
-                        <Button 
-                            variant="contained" 
-                            color="primary"
-                            onClick={submitFilters}
-                        >
-                            Filter
-                        </Button>
-                        <Button 
-                            variant="outlined" 
-                            color="secondary"
-                            onClick={handleReset}
-                        >
-                            Reset
-                        </Button>
+                        <Grid className={classes.filterButtons}>
+                            <Button 
+                                variant="contained" 
+                                color="primary"
+                                onClick={submitFilters}
+                                className={classes.buttons}
+                            >
+                                Filter
+                            </Button>
+                            <Button 
+                                variant="outlined" 
+                                color="secondary"
+                                onClick={handleReset}
+                                className={classes.buttons}
+                            >
+                                Reset
+                            </Button>
+                        </Grid>
                     </Grid>
                 </Paper>
             </Grid>
