ac3c3d180c0302d8bdf5d4c116aaf167d8971609
katrinamarquez <katrina.l.marquez@gmail.com>
2021-01-17 20:07:46 +1000

    update contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,6 +33,8 @@ const Contact = () => {
                 <Typography variant="h6">
                     Contact Us
                 </Typography>
+            </Box>
+            <Box py={4} align="center">
                 <TableContainer component={Paper}>
                     <Table className={classes.table} aria-label="simple table">
                         <TableBody>
@@ -48,18 +50,18 @@ const Contact = () => {
                         </TableBody>
                     </Table>
                 </TableContainer>
-                </Box>
-                <Box py={4} align="center">
-                    <iframe 
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.978764016295!2d153.02095171505695!3d-27.469920482890775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b915a0467886afb%3A0x7e7af927778ca136!2s14%20Adelaide%20St%2C%20Brisbane%20City%20QLD%204000!5e0!3m2!1sen!2sau!4v1610415566345!5m2!1sen!2sau" 
-                        width="600" 
-                        height="450" 
-                        frameborder="0" 
-                        style={{ border: 0 }} 
-                        allowfullscreen="" 
-                        aria-hidden="false" 
-                        tabindex="0">
-                    </iframe>
+            </Box>
+            <Box py={4} align="center">
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.978764016295!2d153.02095171505695!3d-27.469920482890775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b915a0467886afb%3A0x7e7af927778ca136!2s14%20Adelaide%20St%2C%20Brisbane%20City%20QLD%204000!5e0!3m2!1sen!2sau!4v1610415566345!5m2!1sen!2sau" 
+                    width="600" 
+                    height="450" 
+                    frameborder="0" 
+                    style={{ border: 0 }} 
+                    allowfullscreen="" 
+                    aria-hidden="false" 
+                    tabindex="0">
+                </iframe>
             </Box>
         </div>
     );
