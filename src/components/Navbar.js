f306804f86b59de97659053bf3299163e7158a4a
Tyler Hall <tylerhall12@outlook.com>
2021-01-19 16:05:55 +1000

    Added spacing to cart icon, header and footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,8 @@ import {
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1
+        marginBottom: theme.spacing(3),
+        flexGrow: 1
     },
     icon: {
         marginRight: theme.spacing(2),
@@ -101,7 +102,7 @@ const Navbar = ({history}) => {
                                     onClick={() => handleMenuClick('/quote')}
                                     color="inherit"
                                 >
-                                    <ShoppingCartIcon />
+                                    <ShoppingCartIcon fontSize="large" className={classes.icon}/>
                                 </IconButton>
                             )}
                             { isMobile ? (
